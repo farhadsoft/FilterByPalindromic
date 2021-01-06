4f3efddf8cc119ddbcfdabfe70814597d3cfe5d5
Farhad <epam@farhad.su>
2021-01-06 11:58:48 +0300

    All done!

--- FilterByPalindromicTask/ArrayExtension.cs
+++ FilterByPalindromicTask/ArrayExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FilterByPalindromicTask
 {
@@ -16,11 +17,41 @@ namespace FilterByPalindromicTask
         /// <exception cref="ArgumentException">Throw when array is empty.</exception>
         /// <example>
         /// {12345, 1111111112, 987654, 56, 1111111, -1111, 1, 1233321, 70, 15, 123454321}  => { 1111111, 123321, 123454321 }
-        /// {56, -1111111112, 987654, 56, 890, -1111, 543, 1233}  => {  }
+        /// {56, -1111111112, 987654, 56, 890, -1111, 543, 1233}  => {  }.
         /// </example>
         public static int[] FilterByPalindromic(int[] source)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (source is null)
+            {
+                throw new ArgumentNullException($"Throw when array is null. {nameof(source)}");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException($"Throw when array is empty. {nameof(source)}");
+            }
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                int number = source[i];
+                int remineder, sum = 0;
+                int temp = number;
+                while (number > 0)
+                {
+                    remineder = number % 10;
+                    sum = (sum * 10) + remineder;
+                    number /= 10;
+                }
+
+                if (temp == sum)
+                {
+                    result.Add(temp);
+                }
+            }
+
+            return result.ToArray();
         }
     }
 }
